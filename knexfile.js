124f067f6616639d4405f6c81dd5bf686db6a12e
Theo Breuer-Weil <theobreuerweil@gmail.com>
2018-04-01 07:25:09 +0100

    Add knexfile
    Includes test, development and production environment
    configuration

---
+++ knexfile.js
@@ -0,0 +1,48 @@
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: 'postgres://localhost/tigris',
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: __dirname + '/database/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/database/seeds/development'
+    }
+  },
+
+  test: {
+    client: 'postgresql',
+    connection: 'postgres://localhost/tigris-test',
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: __dirname + '/database/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/database/seeds/test'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: __dirname + '/database/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/database/seeds/production'
+    }
+  }
+
+};
