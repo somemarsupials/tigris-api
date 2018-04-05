968f928608f0ca384be64e8df97dfb617b51b2da
Theo Breuer-Weil <theobreuerweil@gmail.com>
2018-04-05 23:51:28 +0300

    Add gulpfile with template-env task
    template-env creates blank version of environment
    YAML files for upload to git.

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const gulp = require('gulp');
+const yaml = require('js-yaml');
+
+function nullify(object) {
+  let nullified = {};
+
+  Object.keys(object).forEach(key => {
+    if (typeof object[key] === 'object') {
+      nullified[key] = nullify(object[key]);
+    }
+    else {
+      nullified[key] = null;
+    };
+  });
+
+  return nullified;
+};
+
+gulp.task('template-env', function () {
+  let template = yaml.dump(nullify(require('./env')));
+  fs.writeFileSync('./env/template.yaml', template);
+});
