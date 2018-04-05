b2e7364ebc58c8dad7ef84237924a4045134d397
Theo Breuer-Weil <theobreuerweil@gmail.com>
2018-04-05 22:25:36 +0300

    Use forEach loop to include routes in app

--- app.js
+++ app.js
@@ -3,4 +3,12 @@ const http = require('http');
 
 const app = express();
 
+const routes = [
+  'auth',
+];
+
+routes.forEach(route => {
+  app.use(`/${route}`, require(`./controllers/${route}`));
+});
+
 module.exports = app;
