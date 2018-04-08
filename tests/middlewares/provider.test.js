1e0e8af973dcf17636c67126074d13d2178cfb0c
Theo Breuer-Weil <theobreuerweil@gmail.com>
2018-04-08 22:36:25 +0300

    Rewrite provider test in Jest

--- tests/middlewares/provider.test.js
+++ tests/middlewares/provider.test.js
@@ -1,24 +1,24 @@
-const test = require('ava').test;
-const sinon = require('sinon');
 const provider = require('../../middlewares/provider');
 
-let req, env, next;
+describe('provider middleware', () => {
+  let req, env, next;
 
-test.beforeEach(() => {
-  env = {
-    providers: {
-      hello: 'world'
+  beforeEach(() => {
+    env = {
+      providers: {
+        hello: 'world'
+      }
     }
-  }
-  req = {};
-  next = sinon.spy();
-  provider('hello')(req, null, next, env);
-});
+    req = {};
+    next = jest.fn();
+    provider('hello')(req, null, next, env);
+  });
 
-test('provider middleware - includes supplied data', t => {
-  t.deepEqual(req.provider, 'world');
-});
+  it('includes supplied data', () => {
+    expect(req.provider).toEqual('world');
+  });
 
-test('provider middleware - calls next', t => {
-  t.true(next.calledOnce);
+  it('provider middleware - calls next', () => {
+    expect(next).toHaveBeenCalledTimes(1);
+  });
 });
