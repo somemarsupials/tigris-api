ec4e0469f5af5fc43b5ff9ba993a15e1c6d867b9
Theo Breuer-Weil <theobreuerweil@gmail.com>
2018-04-08 22:37:14 +0300

    Add json and form-handling middlewares

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const http = require('http');
 
 const app = express();
+
+// middlewares
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// top-level routes
 
 const routes = [
   'auth',
