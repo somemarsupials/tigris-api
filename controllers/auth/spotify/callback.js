1c46d150732af9da4686a8cc35f66723e971383f
Theo Breuer-Weil <theobreuerweil@gmail.com>
2018-04-11 15:59:53 +0100

    Reduce responsibility of /auth/spotify/callback
    Now simply stores code in server-side session and
    redirects user to landing page on client-side.

--- controllers/auth/spotify/callback.js
+++ controllers/auth/spotify/callback.js
@@ -1,28 +1,12 @@
 module.exports = async function (req, res, next) {
-  let authRes;
 
   try {
-    authRes = await req.httpClient({
-      method: 'post',
-      url: req.provider.tokenURI,
-      data: {
-        grant_type: 'authorization_code',
-        code: req.query.code,
-        redirect_uri: req.provider.redirectURI,
-      },
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Authorization': req.provider.bearerBase64,
-      },
-    });
+    req.session.code = req.query.code;
   }
   catch (error) {
-    authRes = error.response || {
-      status: 500,
-      data: { error: error.message },
-    };
+    console.error(error);
   };
 
-  res.status(authRes.status);
-  return res.json(authRes.data);
+  return res.redirect(`${req.origin}/#/auth/spotify/callback`);
 };
+
--- tests/controllers/auth/spotify/callback.test.js
+++ tests/controllers/auth/spotify/callback.test.js
@@ -1,101 +1,31 @@
 const route = require('../../../../controllers/auth/spotify/callback');
 
 describe('/auth/spotify/callback', () => {
-  let req, res, authRes;
+  let req, res;
 
   beforeEach(() => {
-    authRes = {
-      status: 200,
-      data: 'data'
-    };
     req = {
-      body: {
-        callback: 'callback',
-      },
-      provider: {
-        callbackURI: 'callback.com',
-        redirect_uri: 'uri',
-        bearerBase64: 'credentials',
-      },
+      origin: 'site.com',
       query: {
         code: 'code'
       },
-      httpClient: jest.fn().mockReturnValue(authRes)
+      session: {},
     };
-    res = { 
-      status: jest.fn(),
-      json: jest.fn()
-    };
+    res = { redirect: jest.fn() };
   });
 
-  describe('when making API request', () => {
+  describe('when called back', () => {
     beforeEach(() => {
       route(req, res);
     });
 
-    it('makes correct API request', () => {
-      expect(req.httpClient.mock.calls).toMatchSnapshot();
+    it('redirects to correct location', () => {
+      expect(res.redirect)
+        .toHaveBeenCalledWith('site.com/#/auth/spotify/callback');
     });
 
-    it('makes one API request', () => {
-      expect(req.httpClient).toHaveBeenCalledTimes(1);
-    });
-  });
-
-  describe('when error', () => {
-    let error;
-
-    beforeEach( () => {
-      error = new Error();
-      error.response = {
-        status: 404,
-        data: 'error'
-      };
-      req.httpClient = jest.fn().mockImplementation(() => { 
-        throw error;
-      });
-      route(req, res);
-    });
-
-    it('uses error status',  () => {
-      expect(res.status).toHaveBeenCalledWith(404);
-    });
-
-    it('when error, uses error data',  () => {
-      expect(res.json).toHaveBeenCalledWith('error');
-    });
-  });
-
-  describe('when non-http error', () => {
-    let error;
-
-    beforeEach( () => {
-      req.httpClient = jest.fn().mockImplementation(() => { 
-        throw new Error('other error');
-      });
-      route(req, res);
-    });
-
-    it('uses error status',  () => {
-      expect(res.status).toHaveBeenCalledWith(500);
-    });
-
-    it('when error, uses error data',  () => {
-      expect(res.json).toHaveBeenCalledWith({ error: 'other error' });
-    });
-  });
-
-  describe('when successful', () => {
-    beforeEach( () => {
-      route(req, res);
-    });
-
-    it('uses response status',  () => {
-      expect(res.status).toHaveBeenCalledWith(200);
-    });
-
-    it('uses response data',  () => {
-      expect(res.json).toHaveBeenCalledWith('data');
+    it('sets session code', () => {
+      expect(req.session.code).toEqual('code');
     });
   });
 });
