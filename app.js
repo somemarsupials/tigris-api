19f21007d645ea0b4481ab95c2db54ff7af2eeb0
Theo Breuer-Weil <theobreuerweil@gmail.com>
2018-04-11 15:57:51 +0100

    Configure app for CORS and sessions
    Use Redis as backing store for sessions. Redis configuration
    stored in env. CORS origin is configured based on env.

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const express = require('express');
+const env = require('./env');
 const http = require('http');
+const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
 
 const app = express();
 
 // middlewares
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({
+  origin: env.client.url,
+  credentials: true
+}));
+
+app.use(bodyParser.urlencoded({ 
+  extended: true 
+}));
+
+app.use(session({ 
+  store: new RedisStore(env.redis),
+  secret: 'tigger',
+})); 
 
 // top-level routes
 
